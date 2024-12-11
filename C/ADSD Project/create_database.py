4e02e708839baacdb62556e01c6e8f0d0e2a0a5b
Srihari3008 <144619025+Srihari3008@users.noreply.github.com>
2024-12-11 12:57:12 -0500

    Add files via upload

--- C/ADSD Project/create_database.py
+++ C/ADSD Project/create_database.py
@@ -7,8 +7,9 @@ def create_database():
     db.drop_collection("accommodation_collection")
     accommodation_collection = db.accommodation_collection
     accommodation_collection.insert_many([
-        {"name": "Sea View Villa", "location": "Miami", "price": 250.0, "amenities": ["Pool", "Wi-Fi"]},
-        {"name": "Mountain Cabin", "location": "Denver", "price": 150.0, "amenities": ["Fireplace", "Hiking"]}
+        {"name": "The Tree House", "location": "Ohio", "price": 250.0, "amenities": ["Security", "Parking", "Swimming Pool", "Garden Area", "Roof Top"]},
+        {"name": "Historic Homestead", "location": "Kent", "price": 300.0, "amenities": ["Outdoor Space", "Fire Place", "Back Yard", "Wifi"]},
+        {"name": "The Luxury Villa", "location": "Cleveland", "price": 350.0, "amenities": ["Fitness Center", "Fire Place", "In-unit Laundry", "Wifi"]}
     ])
 
     db.drop_collection("review_collection")
